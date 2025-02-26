0873c98322b9251db178e40d2d4e5f2fe822fd6a
Ivan Mikhaylov <imi@cone.ee>
2025-02-26 09:59:59 +0200

    V2 without recursive search and with support of converting Efvo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,14 @@
-import net.sf.jasperreports.engine.{JRException, JasperCompileManager, JasperReport}
-import net.sf.jasperreports.engine.util.{JRLoader, JRSaver}
+import net.sf.jasperreports.engine.JasperCompileManager
+import net.sf.jasperreports.engine.util.JRSaver
 import net.sf.jasperreports.engine.xml.JRXmlLoader
 
-import java.io.{File, FilenameFilter}
+import java.io.{ByteArrayInputStream, File}
+import java.nio.file.{FileSystemException, Files}
 import scala.util.{Failure, Success, Try}
+
 
 object Main {
 
-  implicit class PrintOption[T](option: Option[T]) {
-    def orPrint(message: => String): Option[T] = {
-      if (option.isEmpty) println(message)
-      option
-    }
-  }
   implicit class PrintError[T](result: Try[T]) {
     def orPrint(message: Throwable => String): Option[T] = result match {
       case Success(value) => Some(value)
@@ -20,40 +16,41 @@ object Main {
     }
   }
 
-  object JrxmlFilter extends FilenameFilter {
-    def accept(dir: File, name: String): Boolean = name.endsWith(".jrxml")
+  def readConvertJrxml(source: File): Array[Byte] = {
+    val origXml = Files.readAllBytes(source.toPath)
+    val str = new String(origXml)
+
+    val jsonLocal = "net.sf.jasperreports.json.data.JsonDataSource"
+    val jsonServer = "net.sf.jasperreports.engine.data.JsonDataSource"
+    val jsonReplaced = str.replace(jsonLocal, jsonServer)
+    if (jsonReplaced != str) println(s"${source.getAbsolutePath}: Replaced \"$jsonLocal\" -> \"$jsonServer\"")
+
+    jsonReplaced.getBytes
   }
 
-  def filterValidFiles(files: Iterable[File], depth: Int): Iterable[String] = files.flatMap { file =>
-    if (file.isFile) file.getAbsolutePath :: Nil
-    else if (file.isDirectory && depth > 0) filterValidFiles(file.listFiles(JrxmlFilter), depth - 1)
-    else if (file.isDirectory) Nil
-    else { println(s"File ${file.getAbsolutePath} doesn't exist"); Nil }
-  }
+  def compile(source: File, destination: File): Unit = {
+    val convertedXml = readConvertJrxml(source)
+    Files.write(new File(destination, source.getName).toPath, convertedXml)
+    val input = new ByteArrayInputStream(convertedXml)
+    val sourcePath = source.getAbsolutePath
 
-  def filterValid(paths: Iterable[String], depth: Int): Iterable[String] =
-    filterValidFiles(paths.map(new File(_)), depth)
-
-  def compile(validArgs: Iterable[String]): Unit = {
     for {
-      jrxml <- validArgs
-      outputPath <- Some(jrxml).collect { case s"$prefix.jrxml" => s"$prefix.jasper" }
-        .orPrint(s"$jrxml doesn't end with .jrxml")
-      input = JRLoader.getFileInputStream(jrxml)
+      outputName <- Some(source.getName).collect { case s"$prefix.jrxml" => s"$prefix.jasper" }
       design <- Try { JRXmlLoader.load(input) }
-        .orPrint(e => s"Error loading report: $jrxml - ${e.getMessage}")
+        .orPrint(e => s"$sourcePath: Error loading report - ${e.getMessage}")
       report <- Try { JasperCompileManager.compileReport(design) }
-        .orPrint(e => s"Error compiling report: $jrxml - ${e.getMessage}")
-    } yield Try { JRSaver.saveObject(report, outputPath) }
-      .orPrint(e => s"Error saving compiled report: $jrxml - ${e.getMessage}")
-      .map { _ => println(s"Compiled $jrxml successfully") }
+        .orPrint(e => s"$sourcePath: Error compiling report - ${e.getMessage}")
+    } yield Try { JRSaver.saveObject(report, new File(destination, outputName)) }
+      .orPrint(e => s"$sourcePath: Error saving compiled report - ${e.getMessage}")
+      .map { _ => println(s"$sourcePath: Compiled successfully") }
   }
 
-  def main(args: Array[String]): Unit = if (args.nonEmpty) {
-    val validArgs =
-      if (args.head == "-r") filterValid(args.tail, Int.MaxValue)
-      else filterValid(args, 1)
-    compile(validArgs)
-  } else println("Empty arguments")
+  def main(args: Array[String]): Unit = args.map(new File(_)) match {
+    case Array(source, destination) =>
+      if (source.isFile) compile(source, destination)
+      else if (source.isDirectory) source.listFiles().foreach { f => if (f.isFile) compile(f, destination) }
+      else throw new FileSystemException(s"${source.getAbsolutePath}: Illegal file ")
+    case _ => throw new IllegalArgumentException("Invalid argument number")
+  }
 
 }
