dd447eeafcda44358d0d3fdfddd6b37952f62fb7
Ivan Mikhaylov <imi@cone.ee>
2025-02-27 10:37:46 +0200

    Don't copy file if it's not an jrxml Colored print

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,10 +9,13 @@ import scala.util.{Failure, Success, Try}
 
 object Main {
 
+  def print(file: File, message: String, ansiColor: String): Unit =
+    println(file.getAbsolutePath + ": " + ansiColor + message + Console.RESET)
+
   implicit class PrintError[T](result: Try[T]) {
-    def orPrint(message: Throwable => String): Option[T] = result match {
+    def orPrint(file: File, message: String): Option[T] = result match {
       case Success(value) => Some(value)
-      case Failure(error) => println(message(error)); None
+      case Failure(error) => print(file, message + " - " + error.getMessage, Console.RED); None
     }
   }
 
@@ -23,33 +26,30 @@ object Main {
     val jsonLocal = "net.sf.jasperreports.json.data.JsonDataSource"
     val jsonServer = "net.sf.jasperreports.engine.data.JsonDataSource"
     val jsonReplaced = str.replace(jsonLocal, jsonServer)
-    if (jsonReplaced != str) println(s"${source.getAbsolutePath}: Replaced \"$jsonLocal\" -> \"$jsonServer\"")
+    if (jsonReplaced != str) print(source, s"Replaced \"$jsonLocal\" -> \"$jsonServer\"", Console.YELLOW)
 
     jsonReplaced.getBytes
   }
 
   def compile(source: File, destination: File): Unit = {
     val convertedXml = readConvertJrxml(source)
-    Files.write(new File(destination, source.getName).toPath, convertedXml)
     val input = new ByteArrayInputStream(convertedXml)
-    val sourcePath = source.getAbsolutePath
 
     for {
       outputName <- Some(source.getName).collect { case s"$prefix.jrxml" => s"$prefix.jasper" }
-      design <- Try { JRXmlLoader.load(input) }
-        .orPrint(e => s"$sourcePath: Error loading report - ${e.getMessage}")
-      report <- Try { JasperCompileManager.compileReport(design) }
-        .orPrint(e => s"$sourcePath: Error compiling report - ${e.getMessage}")
+      design <- Try { JRXmlLoader.load(input) }.orPrint(source, "Error loading report - ")
+      report <- Try { JasperCompileManager.compileReport(design) }.orPrint(source, s"Error compiling report - ")
     } yield Try { JRSaver.saveObject(report, new File(destination, outputName)) }
-      .orPrint(e => s"$sourcePath: Error saving compiled report - ${e.getMessage}")
-      .map { _ => println(s"$sourcePath: Compiled successfully") }
+      .orPrint(source, "Error saving compiled report - ")
+      .map { _ => Files.write(new File(destination, source.getName).toPath, convertedXml) }
+      .map { _ => print(source, "Compiled successfully", Console.GREEN) }
   }
 
   def main(args: Array[String]): Unit = args.map(new File(_)) match {
     case Array(source, destination) =>
       if (source.isFile) compile(source, destination)
       else if (source.isDirectory) source.listFiles().foreach { f => if (f.isFile) compile(f, destination) }
-      else throw new FileSystemException(s"${source.getAbsolutePath}: Illegal file ")
+      else print(source, "Illegal file", Console.RED)
     case _ => throw new IllegalArgumentException(s"Invalid argument number (${args.length}), but 2 expected")
   }
 
